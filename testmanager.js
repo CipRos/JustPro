08b547baebd532ba71a46c7954fdd691931be2bb
CipRos <42282352+CipRos@users.noreply.github.com>
2024-09-18 01:13:33 +0300

    Update testmanager.js

--- testmanager.js
+++ testmanager.js
@@ -1,7 +1,1 @@
-var firstlog = true;
-var templog = { logs: "" };
-var temptime = { clock: "" };
-var focused = true;
-
 var JPTestManager = "this is running from a script manager!";
-console.log("idk bruv");
