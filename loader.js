2d80f5bf95f9b66bbdd54aaa299381eb91a9be7c
CipRos <42282352+CipRos@users.noreply.github.com>
2024-09-18 03:14:35 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -2,4 +2,4 @@ console.log("[JP Launcher] Loading...")
 var list = [
   {url: "https://www.google.com/", load: "https://raw.githubusercontent.com/CipRos/JustPro/main/google.js"}
 ]
-fetch(list.find((link) => link.url(document.location.href)).then(r => r.text()).then(r => eval(r))
+fetch(list.find((link) => link.url(document.location.href))).then(r => r.text()).then(r => eval(r))
