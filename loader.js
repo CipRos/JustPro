d8f91c21aaf0f3b6ada6c7d917f0da6994db5505
CipRos <42282352+CipRos@users.noreply.github.com>
2024-09-18 03:11:26 +0300

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,5 @@
+console.log("[JP Launcher] Loading...")
+var list = [
+  {url: "https://www.google.com/", load: "https://raw.githubusercontent.com/CipRos/JustPro/main/google.js"}
+]
+fetch(list.find((link) => link.url(document.location.href)).then(r => r.text()).then(r => eval(r))
